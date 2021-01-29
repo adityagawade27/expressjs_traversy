bbcf018335e4af62d1982d8d3920aec098028dd4
adityagawade27 <aditya.gawade@colorado.edu>
2021-01-28 22:14:46 -0800

    twoi thirds finished :)

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Aditya Gawade',
+        email: 'aditya@example.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Hal Jordan',
+        email: 'hal@lantern.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Bruce Wayne',
+        email: 'bruce@waynecorp.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+const { resolveSoa } = require('dns');
 const express = require('express');
+const { reset } = require('nodemon');
 const path = require('path');
+const members = require('./Members');
+const logger = require('./middleware/logger')
 
 const app = express();
+
+// Init middleware
+//app.use(logger);
+
+
+// Body parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 // Set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Members API routes
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
